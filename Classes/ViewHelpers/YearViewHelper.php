99a06993c887a7c8e4b1bb8cfd803b8ff5da81a4
PazosDev <it-pazos@hispeed.ch>
2016-05-19 16:37:26 +0200

    [TASK] initial commit

---
+++ Classes/ViewHelpers/YearViewHelper.php
@@ -0,0 +1,12 @@
+<?php
+namespace Sesamnet\Snviewhelpers\ViewHelpers;
+
+class YearViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper {
+    
+    public function render()
+        {
+            $content = "Hallo Claudio";
+            return $content;
+        }
+   }
+?>
---
+++ ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "snviewhelpers".
+ *
+ * Auto generated 19-05-2016 15:26
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'sn Viewhelpers',
+	'description' => 'Viewhelpers',
+	'category' => 'viewhelpers',
+	'shy' => 0,
+	'version' => '0.0.1',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Claudio Pazos',
+	'author_email' => 'claudio.pazos@sesamnet.ch',
+	'author_company' => 'sesamnet',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5-7.9.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:0:{}',
+	'suggests' => array(
+	),
+);
