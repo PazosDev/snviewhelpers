912e483e36449502f4b9ed359e1487b5823c56f8
PazosDev <it-pazos@hispeed.ch>
2016-05-23 15:06:08 +0200

    [BUG]

--- Classes/ViewHelpers/FilenameViewHelper.php
+++ Classes/ViewHelpers/FilenameViewHelper.php
@@ -6,36 +6,37 @@ class FilenameViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractTagBas
     
     
     /**
-    * @param string $name
+    * @param string $filename
     * @param int $option
     **/
     
-    public function render($name, $option)
+    /**
+    Option 1 = Nur Dateiname
+    Option 2 = Nur Dateiendung
+    Option 3 = Dateiname + Endung    
+    **/
+    
+    public function render($filename, $option)
         {
-            $explode = explode(".", $name); 
-            if($option == 1)
-            {
-               $content = $explode[0];
-            }
-            else 
-            {
-                if($option == 2)
-                {
-                    $content = $explode[1];
-                }
-                else
-                {
-                    if($option == 3)
-                    {
-                        $content = $explode[0].".".$explode[1];
-                    }
-                    else {
-                        $content = "Fehler: Parameter option darf nur 1 bis 3 sein!";
-                    }
-                    
-                }
-            }
+            $explode = explode(".", $filename); 
             
+            
+            
+            
+            switch ($option) {
+                case 1:
+                    $content = $explode[0];
+                    break;
+                case 2:
+                    $content = $explode[1];
+                    break;
+                case 3:
+                    $content = $explode[0].".".$explode[1];
+                    break;
+                default:
+                    $content = "Fehler: Nur Option 1 - 3 erlaubt!";
+                    break;
+            }
             return $content;
         }
         
